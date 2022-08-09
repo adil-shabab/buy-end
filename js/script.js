84d6d1cd4fcfce31db59110569b549fbe5d8e770
Adil Shabab <adilshaba@gmail.com>
2022-08-09 02:35:05 -0700

    about section completed

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".mySwiper", {
+    effect: "flip",
+    grabCursor: true,
+    autoplay: {
+        delay: 8000,
+        disableOnInteraction: false,
+      },
+    loop: true,
+  });
