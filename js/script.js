711c392cdad7ff8eca78857312506f9b08d6e780
Adil Shabab <adilshabab2004@gmail.com>
2022-10-06 16:59:30 +0530

    watsapp link added

--- js/script.js
+++ js/script.js
@@ -14,6 +14,28 @@ myFunction()
 
 
 
+// feature carousel
+$(document).ready(function(){
+  $('.feature-carousel').owlCarousel({
+      loop:true,
+      margin:10,
+      nav:false,
+      responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:3
+          },
+          1000:{
+              items:3
+          }
+      }
+    })
+});
+
+
+
 
 
 
